68bee7ab9d55bd4cde8b8386f9af68f79ca6b330
Giuseppe Grossi <giuseppe.volpe.grossi@gmail.com>
2024-09-25 18:24:00 -0400

    Removed old function

--- main.py
+++ main.py
@@ -5,46 +5,13 @@ Created on Thu Jul 18 17:18:37 2024
 @author: giuseppe
 """
 import argparse
-import twixtools
 from sigpy import backend  
-from estimation.algos import JointEstimation
 import numpy as np
 import sigpy as sp
 import time
 import sigpy.plot as pl
 
 from estimation import * 
-
-def get_kspaces(filename):
-    multi_twix = twixtools.read_twix(filename, parse_pmu=False)
-    
-    # map the twix data to twix_array objects
-    mapped = twixtools.map_twix(multi_twix)
-    mapped_img_data = mapped[-1]['image']
-    mapped_refscan_data = mapped[-1]['refscan']
-    
-    # make sure that we later squeeze the right dimensions:
-    print(f'Img Data non singleton dims : {mapped_img_data.non_singleton_dims}')
-    print(f'RefScan Data non singleton dims : {mapped_refscan_data.non_singleton_dims}')
-    
-    # remove 2x oversampling and zero pad to ensure same shape
-    mapped_img_data.flags['remove_os'] = True
-    mapped_img_data.flags['zf_missing_lines'] = True
-    mapped_refscan_data.flags['remove_os'] = True
-    mapped_refscan_data.flags['zf_missing_lines'] = True
-    
-    
-    image_ksp = mapped_img_data[:].squeeze()
-    refscan_ksp = mapped_refscan_data[:].squeeze()
-    print(f'Dimensions of image k-space is {image_ksp.shape}')
-    print(f'Dimensions of refscan k-space is {refscan_ksp.shape}')
-    
-    #Rearrange so that shape follows format of [nc, col, par, line]
-    image_ksp = np.transpose(image_ksp, (2, 3, 0, 1))
-    refscan_ksp = np.transpose(refscan_ksp, (2, 3, 0, 1))
-    print(f'New dimenions are [nc, kx, ky, kz] -> {refscan_ksp.shape}')
-
-    return image_ksp, refscan_ksp
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
