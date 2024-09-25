340ce3c98f352691523064661e0fe343706c85fc
Giuseppe Grossi <giuseppe.volpe.grossi@gmail.com>
2024-09-25 11:50:44 -0400

    Re-written algos forgot to include in last commit

--- estimation/algos.py
+++ estimation/algos.py
@@ -1,163 +1,152 @@
-from sigpy.alg import Alg
-from sigpy import backend
-from sigpy.mri.linop import Sense
-from sigpy.mri.app import _estimate_weights
-from sigpy.app import LinearLeastSquares, App
-from estimation.linop import RigidTransform
-from sigpy.linop import Multiply
-import time 
-import sigpy.plot as pl
+import sigpy as sp
 import numpy as np
-class TransformEstimation(Alg):
-    def __init__(self, A, transform_states, image, kspace, max_iter):
-        self.A = A
-        self.rigid_transform = transform_states
-        self.image = image
+from .factors import *
+from .encoding import *
+
+class TransformEstimation(sp.alg.Alg):
+    def __init__(self, mps, masks, transforms, image, kspace, kgrid, kkgrid, rgrid, rkgrid, winic, max_iter):
+        self.mps = mps
+        self.masks = masks
+        self.transforms = transforms
+        self.img = image
         self.kspace = kspace
-        self.winic = 1
-        self.device = backend.get_device(image)
+        self.kgrid = kgrid
+        self.kkgrid = kkgrid
+        self.rgrid = rgrid
+        self.rkgrid = rkgrid
+        self.winic = winic
+        self.device = sp.get_device(image)
+        self.decreasing_err = False
+
+        xp = self.device.xp
+        with self.device:
+            self.gradient = xp.empty((len(transforms), 6))
+            self.hessian = xp.empty((len(transforms),6,6))
         super().__init__(max_iter)
 
     
     def _update(self):
         xp = self.device.xp
         with self.device:
-            
+
+            z_prev = self.transforms
             #Avoid numerical instability
-            if self.winic < 1e-4:
-                self.winic  *= 2
-            if self.winic > 1e16:
-                self.winic /= 2 
+            self.winic = xp.where(self.winic < 1e-4, self.winic * 2, self.winic)
+            self.winic = xp.where(self.winic > 1e16, self.winic / 2, self.winic)
 
             #First forward pass to evaluate at f(z)
-            T = self.rigid_transform.Transform()
-            w = self.A * T * self.image - self.kspace
-            fz = xp.sum(xp.real(xp.conj(w) * w))
+            factors_trans, factors_tan, factors_sin = calc_factors(self.transforms, self.kgrid, self.rkgrid)
+            grad_factors, hess_factors = calc_derivative_factors(self.transforms, self.kgrid, self.kkgrid, self.rkgrid, factors_trans, factors_tan, factors_sin)
+            
+            E = AlignedSense(self.img, self.mps, self.masks, factors_trans, factors_tan, factors_sin) #[channels, shots, kx, ky, kz]
+            w  = (E * self.img) - self.kspace[:, xp.newaxis]
+            w_conj = xp.conj(w)
+            error_prev = xp.sum(xp.real(w * w_conj), axis=(0,2,3,4)) #each shot will have its own error    
 
             #Calculating the first order partial derivated 6 in total
-            gradient = xp.ones(6)
-            first_ord_partials = (self.A * self.rigid_transform.DTransform(l)* self.image for l in range(6))
-            for idx, deriv in enumerate(first_ord_partials):
-                gradient[idx] = xp.sum(xp.real(xp.conj(w) * deriv))
+
+            
+            for l in range(6):
+                gE = GradAlignedSense(self.img, l, self.mps, self.masks, factors_trans, factors_tan, factors_sin, grad_factors, shot_batch_size=None)
+                wl = gE * self.img #[coils, shots, kx, ky, kz]
+                self.gradient[:, l] = xp.sum(xp.real(w_conj * wl) , axis=(0,2,3,4)) #Sum along every dimensions except the shots
             
             #Calculating the second order partials for the Hessian matrix
             #Only the 22 partials of the upper right triangle are calculated
             #then copied to the bottom under the diagonal
-            hessian = xp.zeros((6,6))
+            
             for l, m in xp.nditer(xp.triu_indices(6)):
-                gl = self.A * self.rigid_transform.DTransform(l) * self.image
-                gm = self.A * self.rigid_transform.DTransform(m) * self.image
-                he = self.A * self.rigid_transform.DDTransform(l,m) * self.image
-                hessian[l,m] = xp.sum(xp.real(xp.conj(gl) * gm + xp.conj(w) * he))
+                gl = GradAlignedSense(self.img, l, self.mps, self.masks, factors_trans, factors_tan, factors_sin, grad_factors, shot_batch_size=None) * self.img
+                gm = GradAlignedSense(self.img, m, self.mps, self.masks, factors_trans, factors_tan, factors_sin, grad_factors, shot_batch_size=None) * self.img
+                he = HessAlignedSense(self.img, l, m, self.mps, self.masks, factors_trans, factors_tan, factors_sin, grad_factors, hess_factors, shot_batch_size=None) * self.img
+                self.hessian[:,l,m] = xp.sum(xp.real(xp.conj(gl) * gm + w_conj * he), axis=(0,2,3,4))
+                
             #Copy the upper triangle to the bottom under the diagonal
             lower_idxs = xp.tril_indices(6, k=-1)
-            hessian[lower_idxs]  = hessian.T[lower_idxs]
+            for shot in range(len(self.transforms)):
+                self.hessian[shot][lower_idxs]  = self.hessian[shot].T[lower_idxs]
             
             #Winic adjustment for (27) of paper
-            hessian += self.winic * xp.eye(6)
+            self.hessian += self.winic[:,xp.newaxis,xp.newaxis] * xp.eye(6)
 
             #Newton's step, lstsq is solving for H^-1 * G
-            z_next = self.rigid_transform.parameters - xp.linalg.lstsq(hessian, gradient, rcond=None)[0]
+            #Can only solve one by one for each shot
+            z_next = xp.zeros((len(self.transforms),6))
+            for idx in range(len(self.transforms)):
+                z_next[idx] = z_prev[idx] - xp.linalg.lstsq(self.hessian[idx], self.gradient[idx], rcond=None)[0]
 
             #Restrict ranges so as to not overshoot newton's method
             #Angles
-            z_next[3:][z_next[3:] < -xp.pi] += 2 * xp.pi
-            z_next[3:][z_next[3:] >  xp.pi] -= 2 * xp.pi
+            z_next[:, 3:][z_next[:, 3:] < -xp.pi] += 2 * xp.pi
+            z_next[:, 3:][z_next[:, 3:] >  xp.pi] -= 2 * xp.pi
 
             #Translations
-            min = xp.min(self.rigid_transform.rgrid, axis=(-3,-2,-1))
-            max = xp.max(self.rigid_transform.rgrid, axis=(-3,-2,-1))
-            max_idx = (z_next[:3] > max).nonzero()
-            min_idx = (z_next[:3] < min).nonzero()
-            z_next[max_idx] -= xp.array(self.image.shape)[max_idx]
-            z_next[min_idx] += xp.array(self.image.shape)[min_idx]
+            #min = xp.min(self.rigid_transform.rgrid, axis=(-3,-2,-1))
+            #max = xp.max(self.rigid_transform.rgrid, axis=(-3,-2,-1))
+            #max_idx = (z_next[:3] > max).nonzero()
+            #min_idx = (z_next[:3] < min).nonzero()
+            #z_next[max_idx] -= xp.array(self.image.shape)[max_idx]
+            #z_next[min_idx] += xp.array(self.image.shape)[min_idx]
 
-            self.rigid_transform.update_state(z_next)
-            T = self.rigid_transform.Transform()
-            w = self.A * T * self.image - self.kspace
-            fz_next = xp.sum(xp.real(xp.conj(w) * w))
+            factors_trans, factors_tan, factors_sin = calc_factors(z_next, self.kgrid, self.rkgrid)
+            E = AlignedSense(self.img, self.mps, self.masks, factors_trans, factors_tan, factors_sin) #[channels, shots, kx, ky, kz]
+            w = (E * self.img) - self.kspace[:, xp.newaxis]
+            error_next = xp.sum(xp.real(xp.conj(w) * w), axis=(0,2,3,4))
 
-            if fz_next > fz:
-                self.winic *= 2
-            else:
-                self.winic /= 1.2
+            self.winic = xp.where(error_next >= error_prev, self.winic*2, self.winic/1.2)
 
-class TransformEstimationRecon(App):
-    def __init__(self, A, states, image, kspace, max_iter):
-        self.states = states
-        self.image = image
-        alg = TransformEstimation(A, self.states, image, kspace, max_iter)
-        np.set_printoptions(precision=2)
-        super().__init__(alg, show_pbar=False)
+            self.decreasing_err = (error_next < error_prev).all()
+            if (error_next < error_prev).any():
+                idxs = (error_next < error_prev).nonzero()
+                self.transforms[idxs] = z_next[idx]
 
-    def _output(self):
-        print('-'*40)
-        print(f'Estimated transform parameters are: {self.states.parameters}')
-        print('-'*40)
-        print('\n')
-        return self.states.parameters
 
-    def _post_update(self):
-        print(f'Iteration: {self.alg.iter} -> {self.states.parameters}')
+            z_med = xp.mean(self.transforms, axis=0, keepdims=True)
+            factors_trans, factors_tan, factors_sin = calc_factors(z_med, self.kgrid, self.rkgrid)
+            T = RigidTransform(self.img.shape, factors_trans, factors_tan, factors_sin)
+            self.img = xp.sum(T * self.img, axis=0)
+            self.transforms = z_next - z_med
 
-class JointEstimation(Alg):
-    def __init__(self, kspace, maps, bins, bsize, cg_iter, nm_iter, joint_iter, tol=1e-6):
-
-        self.img_shape = kspace.shape[1:]
-
-        weights = _estimate_weights(kspace, None, None)
-        self.A = Sense(maps, weights=weights, coil_batch_size=None)
+class JointEstimation(sp.alg.Alg):
+    def __init__(self, mps, masks, kspace, kgrid, kkgrid, rgrid, rkgrid, tol=1e-6, max_iter=100):
+        self.x = np.zeros(mps.shape[1:], dtype=np.complex64)
+        self.num_shots = len(masks)
+        self.transforms = np.zeros((self.num_shots, 6), dtype=np.float64)
+        self.mps = mps
+        self.masks = masks
         self.kspace = kspace
-        self.maps = maps
-        self.cg_iter = cg_iter
-        self.nm_iter = nm_iter
-        self.tol = tol
-        self.bins = bins
-        self.bsize = bsize
+        
+        self.winic = np.ones(self.num_shots)
+        self.kgrid = kgrid
+        self.kkgrid = kkgrid
+        self.rgrid = rgrid
+        self.rkgrid = rkgrid
 
-        self.device = backend.get_device(kspace)
-        with self.device:
-            xp = self.device.xp
-            self.tform_parameters = xp.zeros(6, dtype=xp.float64)
-            self.recon_image = xp.zeros(self.img_shape, dtype=xp.complex64)
-            self.xerr = xp.infty
-            p = 1 / (xp.sum(xp.abs(maps)**2, axis=0) + 0.001)
-            self.P = Multiply(self.img_shape, p)
-        super().__init__(joint_iter)
+        p = 1 / (np.sum(np.abs(mps)**2, axis=0) + 0.001)
+        self.P = sp.linop.Multiply(mps.shape[1:], p)
+        self.xerr = np.infty
+        self.tol = tol
+        self.decreasing_err = False
+        super().__init__(max_iter)
 
     def _update(self):
-        print(f'Iteration: {self.iter} with tform {self.tform_parameters}')
 
-        tform_states = RigidTransform(self.tform_parameters, self.img_shape, self.device)
-        xant = self.recon_image
-        self.recon_image = LinearLeastSquares(self.A * tform_states.Transform(), self.kspace, x=self.recon_image, P=self.P, max_iter=self.cg_iter, show_pbar=False).run()
+        prev_x = self.x.copy()
+        factors_trans, factors_tan, factors_sin = calc_factors(self.transforms, self.kgrid, self.rkgrid)
+        E = AlignedSense(self.x, self.mps, self.masks, factors_trans, factors_tan, factors_sin, shot_batch_size=None)
+        #need to modify kspace to have an extra dim for the shots
+        sp.app.LinearLeastSquares(E, np.repeat(self.kspace[:, np.newaxis], self.num_shots, 1), self.x, P=self.P, max_iter=3, tol=1e-6, show_pbar=False).run()
+        
+        t_est_alg = TransformEstimation(self.mps, self.masks, self.transforms, self.x, self.kspace, self.kgrid, self.kkgrid, self.rgrid, self.rkgrid, self.winic, max_iter=1)
+        while not t_est_alg.done():
+            t_est_alg.update()
+        self.transforms = t_est_alg.transforms
+        self.x = t_est_alg.img
+        self.winic = t_est_alg.winic
+        self.decreasing_err = t_est_alg.decreasing_err
+        self.xerr = self.x - prev_x
+        self.xerr = np.real(self.xerr * np.conj(self.xerr))
+        self.xerr = np.max(self.xerr)
 
-        tform_params_temp = []
-        for b in range(self.bins):
-            extraction = np.zeros(self.kspace.shape, dtype=np.complex64)
-            extraction[:, b*self.bsize:(b+1)*self.bsize] = self.kspace[:, b*self.bsize:(b+1)*self.bsize]
-            #pl.ImagePlot(np.squeeze(self.A.H * extraction), title=f'Motion state {b}')
-            tform_states = RigidTransform(self.tform_parameters, self.img_shape, self.device)
-            start = time.perf_counter()
-            t_params = TransformEstimationRecon(self.A, tform_states, self.recon_image, extraction, max_iter=self.nm_iter).run()
-            finish = time.perf_counter()
-            tform_params_temp.append(t_params)
-            print(f'Motion state {b} took {(finish - start):.2f} seconds to complete')
-
-        t_med = np.array(tform_params_temp).mean(0) #Mean params across all the motions states
-        self.recon_image = RigidTransform(t_med, self.img_shape, self.device).Transform() * self.recon_image
-        self.tform_parameters -= t_med
-
-        xp = self.device.xp
-        xant = self.recon_image - xant
-        xant = xp.real(xant*xant.conj())
-        xant = xp.max(xant)
-        self.xerr = xant 
-        #self.xerr = xp.linalg.norm(xant - self.recon_image)
-        print(f'Error is {self.xerr}')
-    
     def _done(self):
-        return (self.iter >= self.max_iter) or (self.xerr <= self.tol)
-
-
-
+        return (self.iter >= self.max_iter) or (self.xerr <= self.tol and self.decreasing_err == True)
