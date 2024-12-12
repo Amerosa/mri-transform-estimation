6a62b4da9818646033b6536fa740c5d6aff5078e
Giuseppe Grossi <giuseppe.volpe.grossi@gmail.com>
2024-12-12 09:35:41 -0500

    Encoding re implemented with cluster gpu support
    The encdoing foward transform now accounts for batching chunks of shots (motion states) to be processed by a single gpu, while also supporting mpi protocols to distribute chunks of these motion states across a distributed computing cluster. Remved old aligend sense linop and hessian version as we are no longer explicitly computed it and instead estimating the hessian matrix with a jacobian approx. The image solver has been greatly simplified with these changes and code bloat has been removed as well.

--- estimation/encoding.py
+++ estimation/encoding.py
@@ -1,37 +1,61 @@
 import sigpy as sp
 import numpy as np
-from .transforms import *
+from .transforms import RigidTransform, GradRigidTransform
+from .factors import calc_factors
 
-def AlignedSense(input, mps, masks, factors_trans, factors_tan, factors_sin, shot_batch_size=None):
+def Encoding(params, mask, mps, kgrid, rkgrid, ishape=None, comm=None, shot_chunk_size=None):
 
-    img_shape = input.shape
-    num_shots = len(masks)
-    if shot_batch_size is None:
-        shot_batch_size = num_shots
-
-    if shot_batch_size < num_shots:
-        num_shot_batches = (num_shots + shot_batch_size - 1) // shot_batch_size
-        E = sp.linop.Vstack(
-            [
-                AlignedSense(
-                    input,
-                    mps,
-                    masks[s * shot_batch_size: (s+1) * shot_batch_size],
-                    factors_trans[s * shot_batch_size: (s+1) * shot_batch_size],
-                    [f[s * shot_batch_size: (s+1) * shot_batch_size] for f in factors_tan],
-                    [f[s * shot_batch_size: (s+1) * shot_batch_size] for f in factors_sin]
-                )
-                for s in range(num_shot_batches)
-            ],
-            axis=1,
-        )
+    num_shots = params.shape[0]
+    if ishape is None:
+        ishape = mps.shape[1:]
+        img_dim = mps.ndim - 1
+    else:
+        img_dim = len(ishape)
     
-    T = RigidTransform(img_shape, factors_trans, factors_tan, factors_sin)
-    S = sp.linop.Multiply(T.oshape, mps[:, np.newaxis])
-    F = sp.linop.FFT(S.oshape, axes=(-3, -2, -1))
-    A = sp.linop.Multiply(F.oshape, masks)
-    E = A * F * S * T
-    return E
+    
+    if shot_chunk_size is None:
+        shot_chunk_size = num_shots
+
+    if shot_chunk_size < num_shots:
+        num_shot_chunks = num_shots // shot_chunk_size
+        A = sp.linop.Add(
+            [
+                Encoding(params[s * shot_chunk_size : (s+1) * shot_chunk_size],
+                         mask[s * shot_chunk_size : (s+1) * shot_chunk_size],
+                         mps,
+                         kgrid,
+                         rkgrid,
+                         ishape=ishape,
+                         comm=comm
+                        )
+                        for s in range(num_shot_chunks)
+            ]
+        )
+
+        if comm is not None:
+            C = sp.linop.AllReduce(A.oshape, comm, in_place=True)
+            A = C * A
+
+        return A
+    
+    device = sp.get_device(params)
+    with device:
+        ftr, ft, fs = calc_factors(params, kgrid, rkgrid)
+
+    T = RigidTransform(ishape, ftr, ft,fs)
+    R = sp.linop.Reshape((T.oshape[0], 1, *T.oshape[1:]), T.oshape)
+    S = sp.linop.Multiply(R.oshape, mps)
+    F = sp.linop.FFT(S.oshape, axes=range(-img_dim, 0))
+    M = sp.linop.Multiply(F.oshape, mask[:, None])
+    Sum = sp.linop.Sum(M.oshape, axes=(0,))
+    A = Sum * M * F * S * R * T
+
+    if comm is not None:
+        C = sp.linop.AllReduce(A.oshape, comm, in_place=True)
+        A = C * A
+
+    A.repr_str = f"[Encoding Model {shot_chunk_size} Chunks]"
+    return A
 
 def GradAlignedSense(input, partial_idx, mps, masks, factors_trans, factors_tan, factors_sin, grad_factors, shot_batch_size=None):
 
@@ -68,47 +92,4 @@ def GradAlignedSense(input, partial_idx, mps, masks, factors_trans, factors_tan,
     A = sp.linop.Multiply(F.oshape, masks)
     E = A * F * S * T
     E.repr_str = f'GradAlignedSense wrt Partial: {partial_idx}'
-    return E
-
-def HessAlignedSense(input, partial_first, partial_second, mps, masks, factors_trans, factors_tan, factors_sin, grad_factors, hess_factors, shot_batch_size=None):
-
-    img_shape = input.shape
-    num_shots = len(masks)
-    if shot_batch_size is None:
-        shot_batch_size = num_shots
-
-    if shot_batch_size < num_shots:
-        num_shot_batches = (num_shots + shot_batch_size - 1) // shot_batch_size
-        E = sp.linop.Vstack(
-            [
-                HessAlignedSense(
-                    input,
-                    partial_first,
-                    partial_second,
-                    mps,
-                    masks[s * shot_batch_size: (s+1) * shot_batch_size],
-                    factors_trans[s * shot_batch_size: (s+1) * shot_batch_size],
-                    [f[s * shot_batch_size: (s+1) * shot_batch_size] for f in factors_tan],
-                    [f[s * shot_batch_size: (s+1) * shot_batch_size] for f in factors_sin],
-                    {'trans': [gf[s * shot_batch_size: (s+1) * shot_batch_size] for gf in grad_factors['trans']],
-                     'tan':   [gf[s * shot_batch_size: (s+1) * shot_batch_size] for gf in grad_factors['tan']],
-                     'sin':   [gf[s * shot_batch_size: (s+1) * shot_batch_size] for gf in grad_factors['sin']]
-                    },
-                    {'trans': [ [hf[s * shot_batch_size: (s+1) * shot_batch_size] for hf in partials] for partials in hess_factors['trans']],
-                     'tan':   [hf[s * shot_batch_size: (s+1) * shot_batch_size] for hf in hess_factors['tan']],
-                     'sin':   [hf[s * shot_batch_size: (s+1) * shot_batch_size] for hf in hess_factors['sin']]
-                    }
-                    
-                )
-                for s in range(num_shot_batches)
-            ],
-            axis=1,
-        )
-    
-    T = HessRigidTransform(partial_first, partial_second, img_shape, factors_trans, factors_tan, factors_sin, grad_factors, hess_factors)
-    S = sp.linop.Multiply(T.oshape, mps[:, np.newaxis])
-    F = sp.linop.FFT(S.oshape, axes=(-3, -2, -1))
-    A = sp.linop.Multiply(F.oshape, masks)
-    E = A * F * S * T
-    E.repr_str = f'Hess Translation wrt Partial: {partial_first}|{partial_second}'
     return E
--- estimation/image_solver.py
+++ estimation/image_solver.py
@@ -1,48 +1,42 @@
 import sigpy as sp
-from .factors import calc_factors
-from .transforms import RigidTransform
+from .encoding import Encoding
 
 class ImageEstimation(sp.app.App):
     def __init__(        
         self,
-        kspace,
+        y,
         mps,
         mask,
         transforms,
         kgrid,
         rkgrid,
-        img=None,
-        constraint=None,
+        x=None,
         P=None,
+        constraint=None,
+        shot_chunk_size=None,
+        max_iter=10,
+        tol=0,
         device=sp.cpu_device,
-        shot_batch_size=1,
-        max_iter=30,
-        tol=1e-6,
+        comm=None,
         show_pbar=True,
         leave_pbar=True,
         ):
         
-        self.kspace = kspace
-        self.mps = mps
-        self.mask = mask
-        self.transforms = transforms
-        self.kgrid = kgrid
-        self.rkgrid = rkgrid
-        self.shot_batch_size = shot_batch_size
-        self.num_shots = mask.shape[0]
-        self.device = device
-        self.constraint = constraint
+        y = sp.to_device(y, device)
         
-        if img is None:
-            self.img = device.xp.zeros(kspace.shape[1:], dtype=kspace.dtype)
-        else:
-            self.img = sp.to_device(img, device)
-        self.kspace = sp.to_device(self.kspace, device)
         if constraint is not None:
-            self.constraint = sp.to_device(self.constraint, device)
-        b = self._prep_kspace()
-        #b = sp.to_device(b, device)
-        alg = sp.alg.ConjugateGradient(self._cg_encoding, b, x=self.img, P=P, max_iter=max_iter, tol=tol)
+            self.constraint = sp.to_device(constraint, device)
+
+        E = Encoding(transforms, mask, mps, kgrid, rkgrid, comm=comm, shot_chunk_size=shot_chunk_size)
+
+        if x is None:
+            with device:
+                x = device.xp.zeros(E.ishape, dtype=y.dtype)
+        else:
+            x = sp.to_device(x, device)
+
+        alg = sp.alg.ConjugateGradient(E.N, E.H*y, x, P=P, max_iter=max_iter, tol=tol)
+
         super().__init__(alg, show_pbar=show_pbar, leave_pbar=leave_pbar)
     
     def _post_update(self):
@@ -57,33 +51,3 @@ class ImageEstimation(sp.app.App):
             self.pbar.set_postfix(
                     obj="{0:.2E}".format(self.alg.resid)
                 )
-
-    def _cg_encoding(self, x):
-        xp = self.device.xp
-        recon = xp.zeros(x.shape, dtype=xp.complex64)
-        num_chunks = -(self.num_shots//-self.shot_batch_size)
-        S = sp.linop.Multiply((self.shot_batch_size, *x.shape), self.mps[:, xp.newaxis])
-        F = sp.linop.FFT(S.oshape, axes=(-3,-2,-1))
-        for chunk in range(num_chunks):
-            shot_idx = slice(chunk * self.shot_batch_size, (chunk+1) * self.shot_batch_size)
-            factors_trans, factors_tan, factors_sin = calc_factors(self.transforms[shot_idx], self.kgrid, self.rkgrid)
-            T = RigidTransform(x.shape, factors_trans, factors_tan, factors_sin)
-            A = sp.linop.Multiply(F.oshape, self.mask[shot_idx])
-            E = T.H * S.H * F.H * A * F * S * T
-            recon += E * x
-        return recon * self.constraint
-    
-    def _prep_kspace(self):
-        xp = self.device.xp
-        prepped_input = xp.zeros(self.kspace.shape[1:], dtype=xp.complex64)
-        num_chunks = -(self.num_shots//-self.shot_batch_size)
-        S = sp.linop.Multiply((self.shot_batch_size, *prepped_input.shape), self.mps[:, xp.newaxis])
-        F = sp.linop.FFT(S.oshape, axes=(-3,-2,-1))
-        for chunk in range(num_chunks):
-            shot_idx = slice(chunk * self.shot_batch_size, (chunk+1) * self.shot_batch_size)
-            factors_trans, factors_tan, factors_sin = calc_factors(self.transforms[shot_idx], self.kgrid, self.rkgrid)
-            T = RigidTransform(prepped_input.shape, factors_trans, factors_tan, factors_sin)
-            A = sp.linop.Multiply(self.kspace[:, xp.newaxis].shape, self.mask[shot_idx])
-            E = T.H * S.H * F.H * A
-            prepped_input += E * self.kspace[:, xp.newaxis]
-        return prepped_input * self.constraint
