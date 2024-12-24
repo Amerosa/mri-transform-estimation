d1c78c67d0f7382d81a07544cfcea855098f3de6
Giuseppe Grossi <giuseppe.volpe.grossi@gmail.com>
2024-12-24 12:29:20 -0500

    Adjusted main script for experiment

--- main.py
+++ main.py
@@ -4,50 +4,39 @@ Created on Thu Jul 18 17:18:37 2024
 
 @author: giuseppe
 """
-import argparse
 import numpy as np
 import sigpy as sp
-import sigpy.plot as pl
 import os
+import yaml
+import sys
 
-import matplotlib.pyplot as plt
-
-from estimation.utils import generate_corrupt_kspace, generate_sampling_mask, resample
+from estimation.utils import resample
 from estimation.factors import generate_transform_grids
 from estimation.joint_recon import MotionCorruptedImageRecon
 from estimation.image_solver import ImageEstimation
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("img", help="Clean image for experiment")
-    parser.add_argument("mps", help="File for sensitivity maps if already generated")
-    parser.add_argument("out", help="Output dir for the reconstructed image")
-    parser.add_argument('transforms', help="File for the simulated transform parameters")
-    args = parser.parse_args()
+    # Load config file
+    config_file = sys.argv[1]
+    with open(config_file, "r") as f:
+        config = yaml.safe_load(f)
 
     #Experiment Setup | Synthetic data sample to be used
-    img = np.load(args.img)
-    mps = np.load(args.mps)
-    transforms = np.load(args.transforms)
-    #from sigpy.mri import sim
-    #from scipy.ndimage import gaussian_filter
-    #img_shape = [32, 32,32]
-    #mps_shape = [10, 32, 32, 32]
-    #img = sp.shepp_logan(img_shape)
-    #img = gaussian_filter(img, 1)
-    #mps = sim.birdcage_maps(mps_shape)
-    num_shots = len(transforms)
-    
-    norm = np.linalg.norm(img)
-    img /= norm
-    mask = generate_sampling_mask(num_shots, img.shape)
-    ksp = generate_corrupt_kspace(img, mps, mask, transforms)
+    ksp = np.load(config["ksp"])
+    mps = np.load(config["mps"])
+    mask = np.load(config["sampling_mask"])
+
+    num_shots = len(mask)
+    img_shape = ksp.shape[1:]
+
+    import sigpy.config
+    sigpy.config.nccl_enabled = False # not sure if its working atm
+    comm = sp.Communicator()
 
     ksp_in = resample(ksp, 2, axes=[-3,-2], is_kspace=True)
     mps_in = resample(mps, 2, axes=[-3,-2], is_kspace=False)
     mask_in = resample(mask, 2, axes=[-3,-2], is_kspace=True)
-    ground_truth = resample(img, 2, axes=[-3,-2])
     
     rss = np.sqrt(np.sum(np.abs(mps_in)**2, axis=0))
     M = rss > (np.max(rss) * 0.1)
@@ -57,23 +46,30 @@ if __name__ == '__main__':
     recon_four, estimates_ratio_four, objs, t_iter = MotionCorruptedImageRecon(ksp_in, 
                                                                  mps_in, 
                                                                  mask_in, 
-                                                                 img.shape,
+                                                                 img_shape,
                                                                  constraint=M, 
                                                                  P=P, 
                                                                  tol=0, 
-                                                                 max_joint_iter=4_000, 
+                                                                 max_joint_iter=config["parameters"]["level_2_iter"], 
                                                                  device=sp.Device(0), 
-                                                                 save_objective_values=True).run()
-    
-    np.save(os.path.join(args.out, "objectives-resolution-lvl2"), objs)
-    np.save(os.path.join(args.out, "params-resolution-lvl2"), t_iter)
+                                                                 save_objective_values=True,
+                                                                 shot_chunk_size=config["parameters"]["shot_chunk_size"],
+                                                                 comm=comm).run()
+    estimates_ratio_four = comm.gatherv(estimates_ratio_four, root=0)
+    if comm.rank == 0:
+        estimates_ratio_four = estimates_ratio_four.reshape((-1,6))
+        xp = sp.get_array_module(estimates_ratio_four)
+        xp.save(os.path.join(config["output_dir"], "estimates_level_2_resolution"), t_iter)
+        xp.save(os.path.join(config["output_dir"], "objective_level_2_resolution"), objs)
+    else:
+        estimates_ratio_four = np.zeros((num_shots, 6), dtype=float)
+    comm.bcast(estimates_ratio_four, root=0)
 
     #SECOND LEVEL RESOLUTION PYRAMID
     #REDUCING BY A RATIO OF 2
     ksp_in = resample(ksp, 1, axes=[-3,-2], is_kspace=True)
     mps_in = resample(mps, 1, axes=[-3,-2], is_kspace=False)
     mask_in = resample(mask, 1, axes=[-3,-2], is_kspace=True)
-    ground_truth = resample(img, 1, axes=[-3,-2])
 
     rss = np.sqrt(np.sum(np.abs(mps_in)**2, axis=0))
     M = rss > (np.max(rss) * 0.1)
@@ -83,17 +79,27 @@ if __name__ == '__main__':
     recon_two, estimates_ratio_two, objs, t_iter = MotionCorruptedImageRecon(ksp_in, 
                                                                mps_in, 
                                                                mask_in, 
-                                                               img.shape, 
+                                                               img_shape, 
                                                                transforms=estimates_ratio_four, 
                                                                constraint=M, 
                                                                P=P, 
                                                                tol=0, 
-                                                               max_joint_iter=1_000, 
+                                                               max_joint_iter=config["parameters"]["level_1_iter"], 
                                                                device=sp.Device(0), 
-                                                               save_objective_values=True).run()
+                                                               save_objective_values=True,
+                                                               shot_chunk_size=config["parameters"]["shot_chunk_size"],
+                                                               comm=comm).run()
 
-    np.save(os.path.join(args.out, "objectives-resolution-lvl1"), objs)
-    np.save(os.path.join(args.out, "params-resolution-lvl1"), t_iter)
+    estimates_ratio_two = comm.gatherv(estimates_ratio_two, root=0)
+    if comm.rank == 0:
+        estimates_ratio_two = estimates_ratio_two.reshape((-1,6))
+        xp = sp.get_array_module(estimates_ratio_two)
+        xp.save(os.path.join(config["output_dir"], "estimates_level_1_resolution"), t_iter)
+        xp.save(os.path.join(config["output_dir"], "objective_level_1_resolution"), objs)
+    else:
+        estimates_ratio_two = np.zeros((num_shots, 6), dtype=float)
+    comm.bcast(estimates_ratio_two, root=0)
+    
     #FINAL LEVEL OF RESOLUTION PYRAMID
     #FULL RESOLUTION IMAGE ESTIMATION ONLY
     rss = np.sqrt(np.sum(np.abs(mps)**2, axis=0))
@@ -101,14 +107,8 @@ if __name__ == '__main__':
     p = 1 / (np.sum(np.real(mps * mps.conj()), axis=0) + 1e-3)
     P = sp.linop.Multiply(mps.shape[1:], p)
 
-    kgrid, _, rgrid, rkgrid = generate_transform_grids(img.shape, device=sp.Device(-1))
+    kgrid, _, rgrid, rkgrid = generate_transform_grids(img_shape, device=sp.Device(-1))
 
-    final_recon = ImageEstimation(ksp, mps, mask, estimates_ratio_two.get(), kgrid, rkgrid, constraint=M, P=P, tol=1e-12, max_iter=100, device=sp.Device(-1)).run()
+    final_recon = ImageEstimation(ksp, mps, mask, estimates_ratio_two.get(), kgrid, rkgrid, constraint=M, P=P, tol=1e-12, max_iter=10, device=sp.Device(-1), shot_chunk_size=1).run()
 
-    error_mask = img - final_recon
-    corrupted_img = np.sum(mps.conj() * sp.ifft(ksp, axes=[-3,-2,-1]), axis=0)
-    mid = final_recon.shape[0] // 2 
-    figure = np.concatenate([np.abs(img[mid]), np.abs(corrupted_img[mid]), np.abs(final_recon[mid]), np.abs(error_mask[mid])], axis=-1)
-
-    np.save(os.path.join(args.out, "comparison_fig"), figure)
-    np.save(os.path.join(args.out, "recon"), final_recon)
+    np.save(os.path.join(config["output_dir"], "final_recon"), final_recon)
